038871f1a5bc8b095e7bad1c3fab7edcc7c3591b
shinth73 <60809725+shinth73@users.noreply.github.com>
2020-07-05 22:17:04 +0900

    20200705

--- rooms/models.py
+++ rooms/models.py
@@ -6,7 +6,7 @@ from users import models as user_models
 
 class AbstractItem(core_models.TimeStampedModel):
 
-    """Abstract Item"""
+    """ Abstract Item """
 
     name = models.CharField(max_length=80)
 
@@ -18,11 +18,36 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
+
+    """ RoomType Model Definition """
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
     pass
 
 
 class Room(core_models.TimeStampedModel):
-    """Room Model Definition"""
+
+    """ Room Model Definition """
 
     name = models.CharField(max_length=140)
     description = models.TextField()
@@ -33,13 +58,15 @@ class Room(core_models.TimeStampedModel):
     guests = models.IntegerField()
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
-    bath = models.IntegerField()
+    baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
-
